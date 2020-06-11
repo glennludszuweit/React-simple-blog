28cf5c12a25a9cfdd884d5440075613df7f3bfa6
glenn <glenn.ludszuweit@gmail.com>
2020-06-12 01:10:04 +0200

    created Posts component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
+import Posts from './components/Posts';
 
 class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: 'Hello React',
+        content:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, rem officia! Nemo nobis id cupiditate.',
+      },
+      {
+        id: 2,
+        title: 'Hello Simple Blog',
+        content:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, rem officia! Nemo nobis id cupiditate.',
+      },
+      {
+        id: 3,
+        title: 'Hello Admin',
+        content:
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, rem officia! Nemo nobis id cupiditate.',
+      },
+    ],
+  };
   render() {
     return (
       <div className='App'>
         <Header />
+        <Posts posts={this.state.posts} />
       </div>
     );
   }
