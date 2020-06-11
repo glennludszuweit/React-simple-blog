32f5ff0eb7317b78a4c4b539f13e1cb1986d44be
glenn <glenn.ludszuweit@gmail.com>
2020-06-12 01:23:55 +0200

    imported react-router-dom and addded slug to posts dev data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Posts from './components/Posts';
@@ -8,18 +9,21 @@ class App extends Component {
     posts: [
       {
         id: 1,
+        slug: 'hello-react',
         title: 'Hello React',
         content:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, rem officia! Nemo nobis id cupiditate.',
       },
       {
         id: 2,
+        slug: 'hello-simple-blog',
         title: 'Hello Simple Blog',
         content:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, rem officia! Nemo nobis id cupiditate.',
       },
       {
         id: 3,
+        slug: 'hello-admin',
         title: 'Hello Admin',
         content:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, rem officia! Nemo nobis id cupiditate.',
