fd7b833eb89348db68e4d2e38171ef37308bba1b
glenn <glenn.ludszuweit@gmail.com>
2020-06-13 14:29:50 +0200

    added title input and quill text are in PostForm component

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -4,10 +4,36 @@ import Quill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 class PostForm extends Component {
+  state = {
+    title: '',
+    content: '',
+  };
+
   render() {
     return (
       <form action='' className='container'>
         <h1>Add new Post</h1>
+        <p>
+          <input
+            type='text'
+            id='form-title'
+            value={this.state.title}
+            onChange={(e) => {
+              this.state({
+                title: e.target.value,
+              });
+            }}
+          />
+        </p>
+        <p>
+          <Quill
+            onChange={(content, delta, source, editor) => {
+              this.state({
+                content: editor.getContents(),
+              });
+            }}
+          />
+        </p>
         <p>
           <button type='submit'>Submit</button>
         </p>
