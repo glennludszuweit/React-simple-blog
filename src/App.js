dd49754f5c187eba93271eac3d6b67bce5598da9
glenn <glenn.ludszuweit@gmail.com>
2020-06-18 16:23:37 +0200

    reset code

--- src/App.js
+++ src/App.js
@@ -1,118 +1,60 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
-import './App.css';
-import Header from './components/Header';
-import PageNotFound from './components/PageNotFound';
-import Posts from './components/Posts';
-import Post from './components/Post';
-import PostForm from './components/PostForm';
-import Message from './components/Message';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Message from "./components/Message";
+import Posts from "./components/Posts";
+import Post from "./components/Post";
+import PostForm from "./components/PostForm";
+import NotFound from "./components/NotFound";
 
-class App extends Component {
-  state = {
-    posts: [
-      {
-        id: 1,
-        slug: 'sample-post',
-        title: 'Sample Post',
-        content: ' Sample Content',
-      },
-    ],
-    message: null,
-  };
+import "./App.css";
 
-  getNewSlugFromTitle = (title) =>
-    encodeURIComponent(title.toLowerCase().split(' ').join('-'));
+const App = (props) => {
+  const [posts, setPosts] = useState([]);
+  const [message, setMessage] = useState(null);
 
-  addNewPost = (post) => {
-    post.id = this.state.posts.length + 1;
-    post.slug = this.getNewSlugFromTitle(post.title);
-    this.setState({
-      posts: [...this.state.posts, post],
-      message: 'saved',
-    });
+  const setFlashMessage = (message) => {
+    setMessage(message);
     setTimeout(() => {
-      this.setState({ message: null });
-    }, 1500);
+      setMessage(null);
+    }, 1600);
   };
 
-  updatePost = (post) => {
-    post.slug = this.getNewSlugFromTitle(post.title);
-    const index = this.state.posts.findIndex((p) => p.id === post.id);
-    const posts = this.state.posts
-      .slice(0, index)
-      .concat(this.state.posts.slice(index + 1));
-    const newPosts = [...posts, post].sort((a, b) => a.id - b.id);
-    this.setState({
-      posts: newPosts,
-      message: 'updated',
-    });
-    setTimeout(() => {
-      this.setState({ message: null });
-    }, 1500);
-  };
-
-  render() {
-    return (
-      <Router>
-        <div className='App'>
-          <Header />
-          {this.state.message && <Message type={this.state.message} />}
-          <Switch>
-            <Route
-              exact
-              path='/'
-              render={() => <Posts posts={this.state.posts} />}
-            />
-            <Route
-              path='/post/:postSlug'
-              render={(props) => {
-                const post = this.state.posts.find(
-                  (post) => post.slug === props.match.params.postSlug
-                );
-                if (post) return <Post post={post} />;
-                else return <PageNotFound />;
-              }}
-            />
-            <Route
-              exact
-              path='/new'
-              render={() => (
-                <PostForm
-                  addNewPost={this.addNewPost}
-                  post={{
-                    id: 0,
-                    slug: '',
-                    title: '',
-                    content: '',
-                  }}
-                />
-              )}
-            />
-            <Route
-              path='/edit/:postSlug'
-              render={(props) => {
-                const post = this.state.posts.find(
-                  (post) => post.slug === props.match.params.postSlug
-                );
-                if (post) {
-                  return <PostForm updatePost={this.updatePost} post={post} />;
-                } else {
-                  return <Redirect to='/' />;
-                }
-              }}
-            />
-            <Route component={PageNotFound} />
-          </Switch>
-        </div>
-      </Router>
+  const addNewPost = (post) => {
+    post.id = posts.length + 1;
+    post.slug = encodeURIComponent(
+      post.title.toLowerCase().split(" ").join("-")
     );
-  }
-}
+    setPosts([...posts, post]);
+    setFlashMessage(`saved`);
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        {message && <Message type={message} />}
+        <Switch>
+          <Route exact path="/" render={() => <Posts posts={posts} />} />
+          <Route
+            path="/post/:postSlug"
+            render={(props) => {
+              const post = posts.find(
+                (post) => post.slug === props.match.params.postSlug
+              );
+              return <Post post={post} />;
+            }}
+          />
+          <Route
+            exact
+            path="/new"
+            render={() => <PostForm addNewPost={addNewPost} />}
+          />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
 
 const Post = ({ post }) => {
   const converter = new QuillDeltaToHtmlConverter(post.content.ops, {});
   const contentHTML = converter.convert();
 
   return (
-    <article className='post container'>
+    <article className="post container">
       <h1>{post.title}</h1>
       <div
-        className='content'
+        className="content"
         dangerouslySetInnerHTML={{ __html: contentHTML }}
       />
     </article>
