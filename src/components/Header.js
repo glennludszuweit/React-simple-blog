3dcd27452ec980ccf94f0169d4d8802104382a47
glenn <glenn.ludszuweit@gmail.com>
2020-06-19 14:14:30 +0200

    load post into form, setting state from props and modify the handlePostForm function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Header = (props) => (
-  <header className='App-header'>
-    <ul className='container'>
+const Header = props => (
+  <header className="App-header">
+    <ul className="container">
       <li>
-        <Link to='/'>Simple Blog</Link>
+        <Link to="/">My Site</Link>
       </li>
       <li>
-        <Link to='/new'>New Post</Link>
+        <Link to="/new">New Post</Link>
       </li>
     </ul>
   </header>
 );
-
 export default Header;
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,54 +1,85 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import Quill from "react-quill";
 
-import "react-quill/dist/quill.snow.css";
-
-const PostForm = ({ addNewPost }) => {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [saved, setSaved] = useState(false);
-
-  const handlePostForm = (event) => {
-    event.preventDefault();
-    if (title) {
-      const post = {
-        title: title,
-        content: content,
-      };
-      addNewPost(post);
-      setSaved(true);
+class PostForm extends Component {
+  state = {
+    post: {
+      id: this.props.post.id,
+      slug: this.props.post.slug,
+      title: this.props.post.title,
+      content: this.props.post.content
+    },
+    saved: false
+  };
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.post.id !== this.props.post.id) {
+      this.setState({
+        post: {
+          id: this.props.post.id,
+          slug: this.props.post.slug,
+          title: this.props.post.title,
+          content: this.props.post.content
+        }
+      });
+    }
+  }
+  handlePostForm = e => {
+    e.preventDefault();
+    if (this.state.post.title) {
+      if (this.props.updatePost) {
+        this.props.updatePost(this.state.post);
+      } else {
+        this.props.addNewPost(this.state.post);
+      }
+      this.setState({ saved: true });
     } else {
       alert("Title required");
     }
   };
-  if (saved === true) {
-    return <Redirect to="/" />;
-  }
-  return (
-    <form className="container" onSubmit={handlePostForm}>
-      <h1>Add a New Post</h1>
-      <p>
-        <label htmlFor="form-title">Title:</label>
-        <br />
-        <input
-          id="form-title"
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
+  render() {
+    if (this.state.saved === true) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <form className="container" onSubmit={this.handlePostForm}>
+        <h1>Add a New Post</h1>
+        <p>
+          <label htmlFor="form-title">Title:</label>
+          <br />
+          <input
+            defaultValue={this.props.title}
+            id="form-title"
+            value={this.state.post.title}
+            onChange={e =>
+              this.setState({
+                post: {
+                  ...this.state.post,
+                  title: e.target.value
+                }
+              })
+            }
+          />
+        </p>
+        <p>
+          <label htmlFor="form-content">Content:</label>
+        </p>
+        <Quill
+          defaultValue={this.state.post.content}
+          onChange={(content, delta, source, editor) => {
+            this.setState({
+              post: {
+                ...this.state.post,
+                content: editor.getContents()
+              }
+            });
+          }}
         />
-      </p>
-      <p>
-        <label htmlFor="form-content">Content:</label>
-      </p>
-      <Quill
-        onChange={(content, delta, source, editor) => {
-          setContent(editor.getContents());
-        }}
-      />
-      <p>
-        <button type="submit">Save</button>
-      </p>
-    </form>
-  );
-};
+        <p>
+          <button type="submit">Save</button>
+        </p>
+      </form>
+    );
+  }
+}
 export default PostForm;
